cd707d9078cba9605a5b744625c255901114b549
vlad158166 <55807817+vlad158166@users.noreply.github.com>
2023-08-10 23:26:57 +0300

    Add files via upload

---
+++ micro.github.io-main/access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "16.11.2006"; // Дата рождения
+var fullname = "Поліщук Матвій Констянтинович" // ФИО
+var name = "Матвій" // Имя
+
+// Паспорт
+var passport_id = "008888397" // Номер паспорта
+
+// КПП
+var kpp_id = "0345856795" // ИНН
+
+// Права
+var rights_valid_until = "01.01.2022" // Права "Дійсні до"
+var rights_categories = "A, B" // Права "Категорії"
+var rights_tsc = "ТСЦ 8631" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
